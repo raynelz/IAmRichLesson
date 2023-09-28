dfdab9b9c19c5e1a5bc986f159dc187f1be8af78
Zahar Litvinchuk <97597336+raynelz@users.noreply.github.com>
2023-09-28 18:10:28 +0300

    Initial Commit

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Захар Литвинчук on 28.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
